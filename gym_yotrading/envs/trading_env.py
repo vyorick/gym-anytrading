86838b403da5d970aaf14781836900580dafdc54
Vladimir Lavrentiev <vladimir.lavrentiev@gmail.com>
2020-01-20 22:16:03 +0400

    вынес классы

--- gym_yotrading/envs/trading_env.py
+++ gym_yotrading/envs/trading_env.py
@@ -1,5 +1,4 @@
 import logging
-from enum import Enum
 
 import gym
 import matplotlib.pyplot as plt
@@ -8,6 +7,8 @@ from gym import spaces
 from gym.utils import seeding
 
 # Create a custom logger
+from .trading_policy import TradingFSM, Actions, Positions
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 # Create handlers
@@ -17,49 +18,6 @@ c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
 # Add handlers to the logger
 logger.addHandler(c_handler)
-
-
-class Actions(Enum):
-    Sell = 2
-    Buy = 1
-    Nothing = 0
-
-
-class Positions(Enum):
-    Long = 1
-    Short = 2
-    Out = 0
-
-
-class StateElement:
-    def __init__(self, old_position, action, new_position):
-        self.old_position = old_position
-        self.action = action
-        self.new_position = new_position
-        self.is_trade_start = old_position == Positions.Out and new_position != Positions.Out
-        self.is_trade_end = old_position != Positions.Out and new_position == Positions.Out
-
-    def __str__(self):
-        return f"old: {self.old_position}, action: {self.action}, new: {self.new_position}, start: {self.is_trade_start}, end:{self.is_trade_end}"
-
-
-class TradingFSM:
-    def __init__(self):
-        self._states = [StateElement(Positions.Out, Actions.Buy, Positions.Long),
-                        StateElement(Positions.Out, Actions.Sell, Positions.Short),
-                        StateElement(Positions.Out, Actions.Nothing, Positions.Out),
-                        StateElement(Positions.Long, Actions.Buy, Positions.Long),
-                        StateElement(Positions.Long, Actions.Sell, Positions.Out),
-                        StateElement(Positions.Long, Actions.Nothing, Positions.Long),
-                        StateElement(Positions.Short, Actions.Buy, Positions.Out),
-                        StateElement(Positions.Short, Actions.Sell, Positions.Short),
-                        StateElement(Positions.Short, Actions.Nothing, Positions.Short)]
-
-    def get_state(self, old_position, action):
-        for state in self._states:
-            if old_position == state.old_position and action == state.action.value:
-                return state
-        raise Exception('State not found!')
 
 
 class TradingEnv(gym.Env):
