2a422c7a4dd40ee3eb6425a8ae5180763fe7dc53
Vladimir Lavrentiev <vladimir.lavrentiev@gmail.com>
2020-01-23 18:03:46 +0400

    Подготовка к введению пенальти за бездействие

--- gym_yotrading/envs/trading_policy.py
+++ gym_yotrading/envs/trading_policy.py
@@ -15,28 +15,31 @@ class Positions(Enum):
 
 
 class StateElement:
-    def __init__(self, old_position, action, new_position):
+    def __init__(self, old_position, action, new_position, penalty_steps=None):
         self.old_position = old_position
         self.action = action
         self.new_position = new_position
+        self.penalty_after_steps_count = penalty_steps
         self.is_trade_start = old_position == Positions.Out and new_position != Positions.Out
         self.is_trade_end = old_position != Positions.Out and new_position == Positions.Out
 
     def __str__(self):
-        return f"old: {self.old_position}, action: {self.action}, new: {self.new_position}, start: {self.is_trade_start}, end:{self.is_trade_end}"
+        return f"old: {self.old_position}, action: {self.action}, new: {self.new_position}," \
+               f" start: {self.is_trade_start}, end:{self.is_trade_end}, penalty after steps:{self.penalty_after_steps_count}"
 
 
 class TradingFSM:
     def __init__(self):
         self._states = [StateElement(Positions.Out, Actions.Buy, Positions.Long),
                         StateElement(Positions.Out, Actions.Sell, Positions.Short),
-                        StateElement(Positions.Out, Actions.Nothing, Positions.Out),
+                        StateElement(Positions.Out, Actions.Nothing, Positions.Out, 5),
                         StateElement(Positions.Long, Actions.Buy, Positions.Long),
                         StateElement(Positions.Long, Actions.Sell, Positions.Out),
                         StateElement(Positions.Long, Actions.Nothing, Positions.Long),
                         StateElement(Positions.Short, Actions.Buy, Positions.Out),
                         StateElement(Positions.Short, Actions.Sell, Positions.Short),
-                        StateElement(Positions.Short, Actions.Nothing, Positions.Short)]
+                        StateElement(Positions.Short, Actions.Nothing, Positions.Short)
+                        ]
 
     def get_state(self, old_position, action):
         for state in self._states:
